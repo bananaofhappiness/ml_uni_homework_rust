ed769ce5b80e94e084e0ab8e619801db72f436e0
Egor Kurtashkin <egorkurtashkin@gmail.com>
2025-03-11 15:39:27 +0300

    15 task and colab support

--- HW01/polars/test.rs
+++ HW01/polars/test.rs
@@ -51,4 +51,42 @@ fn main() {
                     )
                     .collect()
                     .unwrap();
+
+                // если у нас больше 3 столбцов, то считать mean вручную тупо, поэтому можно так
+// (украл у deepseek, так как документация polars rust просто ужасна)
+
+let score_columns = df
+.clone()
+.schema()
+.iter_names_cloned()
+.filter(|name| name.ends_with("score"))
+.collect::<Vec<_>>();
+
+let sum_expr = score_columns
+.iter()
+.fold(lit(0.0), |acc, col_name| acc + col(PlSmallStr::from_str(col_name)).cast(DataType::Float64));
+
+
+let count = score_columns.len() as f64;
+let average_expr = (sum_expr / lit(count)).alias("average");
+
+let df = df.lazy()
+                    .with_column(average_expr.clone())
+                    .with_column(
+                        when(col("average").lt(50.0))
+                            .then(lit("F"))
+                            .when(col("average").lt(60.0))
+                            .then(lit("E"))
+                            .when(col("average").lt(70.0))
+                            .then(lit("D"))
+                            .when(col("average").lt(80.0))
+                            .then(lit("C"))
+                            .when(col("average").lt(90.0))
+                            .then(lit("B"))
+                            .otherwise(lit("A"))
+                            .alias("Grade")
+                    )
+                    .drop(["average"])
+                    .collect()
+                    .unwrap();
 }
